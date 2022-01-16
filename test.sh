d4d16251ba605303cdd214603ce4b6071c6bfafd
kazukousen <mmchari.0228@gmail.com>
2022-01-16 16:24:51 +0900

    remove redundancy blocks

--- test.sh
+++ test.sh
@@ -63,22 +63,22 @@ echo ""
 
 echo "if"
 echo ""
-assert 5 'func main() int { {var i = 5; if i == 5 { return i}; return 0; } }'
-assert 5 'func main() int { { if i := 5; i == 5 { return i}; return 0; } }'
-assert 5 'func main() int { {var i = 5; if i == 5 { return i} else { return 3} } }'
-assert 3 'func main() int { {var i = 3; if i == 5 { return i} else { return 3} } }'
-assert 3 'func main() int { {var i = 3; if i == 5 { return i} else if i == 3 { return 3} } }'
-assert 3 'func main() int { { i := 1; if i == 5 { return i} else if i == 4 { return 4} else { i = 3 } return i } }'
-assert 4 'func main() int { { i := 4; if i == 5 { return i} else if i == 4 { return 4} else { i = 3 } return i } }'
+assert 5 'func main() int { var i = 5; if i == 5 { return i}; return 0;  }'
+assert 5 'func main() int { if i := 5; i == 5 { return i}; return 0; }'
+assert 5 'func main() int { var i = 5; if i == 5 { return i} else { return 3}  }'
+assert 3 'func main() int { var i = 3; if i == 5 { return i} else { return 3}  }'
+assert 3 'func main() int { var i = 3; if i == 5 { return i} else if i == 3 { return 3}  }'
+assert 3 'func main() int { i := 1; if i == 5 { return i} else if i == 4 { return 4} else { i = 3 } return i }'
+assert 4 'func main() int { i := 4; if i == 5 { return i} else if i == 4 { return 4} else { i = 3 } return i }'
 echo ""
 
 echo "for"
 echo ""
-assert 55 'func main() int { { i:=0; j:=0; for i=0; i<=10; i=i+1 { j=i+j }; return j; } }'
-assert 55 'func main() int { { i:=0; j:=0; for ; i<=10; i=i+1 { j=i+j }; return j; } }'
-assert 55 'func main() int { { i:=0; j := 0; for ; i<=10; { j=i+j; i=i+1 }; return j; } }'
-assert 55 'func main() int { { i := 0; j := 0; for i<=10 { j=i+j; i=i+1 }; return j; } }'
-assert 3 'func main() int { { for {return 3;} return 5; } }'
+assert 55 'func main() int { i:=0; j:=0; for i=0; i<=10; i=i+1 { j=i+j }; return j; }'
+assert 55 'func main() int { i:=0; j:=0; for ; i<=10; i=i+1 { j=i+j }; return j; }'
+assert 55 'func main() int { i:=0; j := 0; for ; i<=10; { j=i+j; i=i+1 }; return j; }'
+assert 55 'func main() int { i := 0; j := 0; for i<=10 { j=i+j; i=i+1 }; return j; }'
+assert 3 'func main() int { for {return 3;} return 5; }'
 echo ""
 
 echo "function call"
